dfdd30f1838eeb205535455116358b9e2186601d
Solanar <and_555@hotmail.com>
2015-12-16 14:11:32 -0700

    use django templatetag capfirst on verbose_name in case verbose_name is not explicitly set

--- templatetags/verbose_name.py
+++ templatetags/verbose_name.py
@@ -1,5 +1,6 @@
 from django.core import exceptions
 from django import template
+from django.template import defaultfilters
 
 register = template.Library()
 
@@ -7,6 +8,12 @@ register = template.Library()
 @register.simple_tag
 def verbose_field_name(instance, field_name):
     try:
-        return instance._meta.get_field(field_name).verbose_name
+        verbose_field_name = defaultfilters.capfirst(
+            instance._meta.get_field(field_name).verbose_name
+        )
     except exceptions.FieldDoesNotExist:
-        return instance.get_all_fields()[field_name].field.verbose_name
+        verbose_field_name = defaultfilters.capfirst(
+            instance.get_all_fields()[field_name].field.verbose_name
+        )
+
+    return verbose_field_name
