53611b11273ee6331d4ca17559e5df1a87a1e870
Solanar <and_555@hotmail.com>
2016-11-15 14:21:26 -0700

    fix fallback/pipeline in devl/debug
    - figured out why there was a case for DEBUG even though the previous was wrong, when not PIPELINE_ENABLED (usually PIPELINE_ENABLED = not DEBUG), django pipeline outputs all source files instead of combining them. We look for debug_fallback_keys for unique keys for fallback load/shim, otherwise try to just slugify the url
    - add get_item filter for dict lookup

--- utils/templatetags/template_utils.py
+++ utils/templatetags/template_utils.py
@@ -10,6 +10,11 @@ def get_attr(obj, attr):
     return getattr(obj, attr)
 
 
+@register.filter
+def get_item(dict_, key):
+    return dict_[key]
+
+
 @register.assignment_tag
 def get_app_settings(app_name):
     app = importlib.import_module(app_name + '.settings')
