d107df803e792a0ae76287b5aacd6a10277cbcba
Solanar <and_555@hotmail.com>
2017-12-27 19:13:09 -0700

    updates
    - use a better layout for default settings
    - add bootstrap-picker versions
    - add base_template setting
    - use https
    - drop '/' from self closing tags
    - add scroll to top btn
    - new way of initializing datepicker
    - add glyphicon to date field
    - remove enctype from create.html
    - default to get_absolute_url for cancel btn
    - add disable_create flag in list.html
    - add prelist block to list.html

--- utils/conf.py
+++ utils/conf.py
@@ -1,15 +1,40 @@
-from django.conf import settings
+from django.conf import settings as django_settings
 
-VERSIONS = {
-    'fallback_js': '1.1.8',
-    'bootstrap_css': '3.3.6',
-    'bootstrap_js': '3.3.6',
-    'html5shiv_js': '3.7.3',
-    'respond_js': '1.4.2',
-    'font_awesome_css': '4.6.3',
-    'jquery_js': '2.2.4',
-    'jquery_ui_css': '1.11.4',
-    'jquery_ui_js': '1.11.4',
-    'jquery_lazy_js': '1.7.0',
+
+utils = 'UTILS'
+base = 'BASE'
+generics = 'GENERICS'
+versions = 'VERSIONS'
+
+settings = {
+    base: {
+        'bootstrap3': False,
+        'fallback': False,
+        'font_awesome': False,
+        'jquery_ui': False,
+        'pipeline': False,
+    },
+    generics: {
+        'base_template': 'base.html',
+    },
+    versions: {
+        'fallback_js': '1.1.8',
+        'bootstrap_css': '3.3.6',
+        'bootstrap_js': '3.3.6',
+        'bootstrap-picker_js': '1.7.1',
+        'bootstrap-picker_css': '1.7.1',
+        'html5shiv_js': '3.7.3',
+        'respond_js': '1.4.2',
+        'font_awesome_css': '4.6.3',
+        'jquery_js': '2.2.4',
+        'jquery_ui_css': '1.11.4',
+        'jquery_ui_js': '1.11.4',
+        'jquery_lazy_js': '1.7.0',
+    },
 }
-VERSIONS.update(getattr(settings, 'VERSIONS', {}))
+
+django_settings_UTILS = getattr(django_settings, utils, {})
+
+settings[base].update(django_settings_UTILS.get(base, {}))
+settings[generics].update(django_settings_UTILS.get(generics, {}))
+settings[versions].update(django_settings_UTILS.get(versions, {}))
