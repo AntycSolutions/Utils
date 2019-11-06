733f2f34fea72f961153f15a2850005110212b4d
Solanar <andrew.charles@antyc.ca>
2019-11-06 11:27:39 -0700

    add git info and json to error emails

--- utils/middleware/exception.py
+++ utils/middleware/exception.py
@@ -1,4 +1,10 @@
+# PSL
+import json
+# 3rd Party
+from django import http
 from django.utils import deprecation
+# Local
+from utils import utils
 
 
 if not hasattr(deprecation, 'MiddlewareMixin'):
@@ -28,9 +34,39 @@ class ExceptionUserInfoMiddleware(deprecation.MiddlewareMixin):
 
         if request.user.is_authenticated():
             if request.user.username:
-                request.META['USER_USERNAME'] = request.user.username
+                request.META['!!_USER_USERNAME'] = request.user.username
             if request.user.email:
-                request.META['USER_EMAIL'] = request.user.email
+                request.META['!!_USER_EMAIL'] = request.user.email
             full_name = request.user.get_full_name()
             if full_name:
-                request.META['USER_FULL_NAME'] = full_name
+                request.META['!!_USER_FULL_NAME'] = full_name
+
+        branch, commit_hash = utils.get_git_info()
+        request.META['!!_PY_BRANCH'] = branch
+        request.META['!!_PY_COMMIT'] = commit_hash
+
+        if hasattr(request, 'content_type'):
+            content_type = request.content_type
+        else:
+            content_type = request.META.get('CONTENT_TYPE')
+        if request.is_ajax() or 'json' in content_type:
+            try:
+                body = request.body.decode()
+            except http.request.UnreadablePostError:
+                request.META['!!_ERROR'] = 'UnreadablePostError'
+                return
+            if not body:
+                request.META['!!_ERROR'] = 'no body'
+                return
+            try:
+                event = json.loads(body)
+            except json.JSONDecodeError:
+                request.META['!!_ERROR'] = 'JSONDecodeError'
+                return
+            if not event:
+                request.META['!!_ERROR'] = 'no json'
+                return
+            config_name = event.get('config_name')
+            if config_name:
+                request.META['!!_CONFIG'] = config_name
+            request.META['!!_JSON'] = json.dumps(event, sort_keys=True)
